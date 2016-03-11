2535ce6b11c9572183d3acb895c2ed008e1dd21c
nshevchenko <nicola.shevchenko@gmail.com>
2016-03-11 06:26:21 +0000

    import sql damn stuff deleted

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -20,15 +20,3 @@ INSERT INTO USERS (id, username, password, loggedIn) VALUES (-2, 'nik', 'asdf', 
 
 INSERT INTO ChatMessages (id, username, message) VALUES (-1, 'henrik', 'hi HENRIK -1 ');
 INSERT INTO ChatMessages (id, username, message) VALUES (-2, 'nik', 'hi NIK -2 ');
-INSERT INTO ChatMessages (id, username, message) VALUES (0, 'henrik', 'hi HENRIK 0');
-INSERT INTO ChatMessages (id, username, message) VALUES (1, 'nik', 'hi NIK 1');
-INSERT INTO ChatMessages (id, username, message) VALUES (2, 'henrik', 'hi HENRIK 2 ');
-INSERT INTO ChatMessages (id, username, message) VALUES (3, 'nik', 'hi NIK3');
-INSERT INTO ChatMessages (id, username, message) VALUES (4, 'henrik', 'hi HENRIK 4');
-INSERT INTO ChatMessages (id, username, message) VALUES (5, 'nik', 'hi NIK5');
-INSERT INTO ChatMessages (id, username, message) VALUES (6, 'henrik', 'hi HENRIK6');
-INSERT INTO ChatMessages (id, username, message) VALUES (7, 'nik', 'hi NIK   7');
-INSERT INTO ChatMessages (id, username, message) VALUES (8, 'henrik', 'hi HENRIK  8');
-INSERT INTO ChatMessages (id, username, message) VALUES (9, 'nik', 'hi NIK   9');
-INSERT INTO ChatMessages (id, username, message) VALUES (10, 'henrik', 'hi HENRIK  10');
-INSERT INTO ChatMessages (id, username, message) VALUES (11, 'nik', 'hi NIK   11');
